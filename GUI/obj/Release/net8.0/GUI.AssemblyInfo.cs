99c87c49ccbd6096a6406fcd1cf22ad83c767862
Jakub <jakub26800@gmail.com>
2024-05-08 11:17:20 +0200

    Created gui pages

--- GUI/obj/Release/net8.0/GUI.AssemblyInfo.cs
+++ GUI/obj/Release/net8.0/GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff323c8b781443a021d7fb91b8d66df5c4d6a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
