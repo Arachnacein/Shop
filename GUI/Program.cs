be2ff20db81b6b212afc06df9c636497589497a7
Jakub <jakub26800@gmail.com>
2024-05-13 13:05:58 +0200

    Added dark theme toggel button

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<MudThemeProvider>();
 builder.Services.AddMudServices(config =>
 {
 
