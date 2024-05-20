7f45d4d467748b36797206cd0ed9be1af983b6c6
Jakub <jakub26800@gmail.com>
2024-05-20 16:10:00 +0200

    Rebuild dll

--- GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net8.0/GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ff323c8b781443a021d7fb91b8d66df5c4d6a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39998ed9f9b6ccb9c597cbf3b46a9ecbfc6887d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
