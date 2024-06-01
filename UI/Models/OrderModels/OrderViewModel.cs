ac3cc8e17e459ab2f613acaee3e1907a955c380d
Jakub <jakub26800@gmail.com>
2024-06-01 12:38:57 +0200

    Added order list, edit order dialog, add order dialog and delete order dialog

---
+++ UI/Models/OrderModels/OrderViewModel.cs
@@ -0,0 +1,14 @@
+namespace UI.Models
+{
+   public class OrderViewModel
+   {
+        public int Id { get; set; }
+        public int Id_Product { get; set; }
+        public Guid Id_User { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public bool Finished { get; set; }
+    }
+}
---
+++ UI/Models/ProductModels/ProductNameViewModel.cs
@@ -0,0 +1,8 @@
+namespace UI.Models
+{
+    public class ProductNameViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
